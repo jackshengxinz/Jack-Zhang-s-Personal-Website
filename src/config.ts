b77e6e606552b68b2e75013b7e004e46810701b8
shengxinz <113316347+shengxinz@users.noreply.github.com>
2025-06-25 22:19:48 +0800

    website1

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Jack Zhang | Website';
+export const SITE_DESCRIPTION = 'Personal portfolio of Jack Zhang, a Computer Engineering student at UIUC with a strong background in statistical modeling, deep learning, and full-stack development. Showcasing research in fairness-aware machine learning, speaker recognition, and hands-on experience in data-driven systems and intelligent applications.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
