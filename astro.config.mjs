4cefb80d9587766c86c12b497ca47837aa16ae87
shengxinz <113316347+shengxinz@users.noreply.github.com>
2025-06-25 23:32:47 +0800

    hi

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
+  base: '/Jack-Zhang-s-Personal-Website/',
+  output: 'static',
   integrations: [mdx(), sitemap(), tailwind()]
 });
